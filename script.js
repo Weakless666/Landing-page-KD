dc43e1478115efd8337afa6b225816fc1de40fbc
Petarpg <petarivvvvv@gmail.com>
2025-01-26 21:45:02 +0200

    Patch 1.2 - G
    Changed the structure of the website's media and fixed some response design mistakes.

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   anchor.addEventListener("click", function (e) {
     e.preventDefault();
-
     const targetId = this.getAttribute("href");
     const targetSection = document.querySelector(targetId);
-
     targetSection.scrollIntoView({
       behavior: "smooth",
     });
