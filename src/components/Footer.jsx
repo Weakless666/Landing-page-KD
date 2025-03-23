88fe5860666a2494bbd4319015e7f17b31046428
Petarpg <petarivvvvv@gmail.com>
2025-03-23 17:10:50 +0200

    Patch 3.1 - Migrate to React! - PP
    Stabilization

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import rvsLogo from '../assets/Images/RVS.png';
-import tiktokIcon from '../assets/Icons/TikTok.png';
+import React from "react";
+import rvsLogo from "../assets/Images/RVS.png";
+import tiktokIcon from "../assets/Icons/TikTok.png";
 
 function Footer() {
   return (
@@ -17,45 +17,61 @@ function Footer() {
         <div className="footer">
           <p className="footer-text">Свържете се с мен чрез:</p>
           <div className="social-icons">
-            <a 
-              href="https://www.tiktok.com/@krasenusa" 
-              className="social-link" 
+            <a
+              href="https://www.tiktok.com/@krasenusa"
+              className="social-link"
               target="_blank"
               rel="noopener noreferrer"
             >
               <img src={tiktokIcon} alt="TikTok" className="social-icon" />
             </a>
-            <a 
-              href="https://www.facebook.com/krasen.diqnov/" 
-              className="social-link" 
+            <a
+              href="https://www.facebook.com/krasen.diqnov/"
+              className="social-link"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <img src="/src/assets/Icons/Facebook.png" alt="Facebook" className="social-icon" />
+              <img
+                src="/src/assets/Icons/Facebook.png"
+                alt="Facebook"
+                className="social-icon"
+              />
             </a>
-            <a 
-              href="https://www.instagram.com/krasenveselinov?igsh=dW1jYnRleTMzOTI3" 
-              className="social-link" 
+            <a
+              href="https://www.instagram.com/krasenveselinov?igsh=dW1jYnRleTMzOTI3"
+              className="social-link"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <img src="/src/assets/Icons/Instagram.png" alt="Instagram" className="social-icon" />
+              <img
+                src="/src/assets/Icons/Instagram.png"
+                alt="Instagram"
+                className="social-icon"
+              />
             </a>
-            <a 
-              href="https://youtube.com/@kraskatamomche?si=sPA1xxazlPHxl3BQ" 
-              className="social-link" 
+            <a
+              href="https://youtube.com/@kraskatamomche?si=sPA1xxazlPHxl3BQ"
+              className="social-link"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <img src="/src/assets/Icons/YouTube.png" alt="YouTube" className="social-icon" />
+              <img
+                src="/src/assets/Icons/YouTube.png"
+                alt="YouTube"
+                className="social-icon"
+              />
             </a>
-            <a 
-              href="https://discord.gg/dxbFjTA9" 
-              className="social-link" 
+            <a
+              href="https://discord.gg/dxbFjTA9"
+              className="social-link"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <img src="/src/assets/Icons/discord.png" alt="Discord" className="social-icon" />
+              <img
+                src="/src/assets/Icons/discord.png"
+                alt="Discord"
+                className="social-icon"
+              />
             </a>
           </div>
         </div>
@@ -64,4 +80,4 @@ function Footer() {
   );
 }
 
-export default Footer; 
+export default Footer;
